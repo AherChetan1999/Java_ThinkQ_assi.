a9f42f3ea70dc78ac878dd7ceac719e9b58e6baf
Aher Chetan <69471574+AherChetan1999@users.noreply.github.com>
2021-09-13 19:11:01 +0530

    1. Check number is even or odd

---
+++ Check_EvenOdd.java
@@ -0,0 +1,21 @@
+package Welcome;
+
+public class Check_EvenOdd {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int num=35;
+		if(num%2==0)
+		{
+			System.out.println(num+" is even");
+		}
+		else
+		{
+			System.out.println(num+" is odd");
+
+		}
+
+	}
+
+}
