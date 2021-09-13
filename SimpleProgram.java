18cb757a47bbf9fa50d8b007ca9ada796f4d23c6
Aher Chetan <69471574+AherChetan1999@users.noreply.github.com>
2021-09-13 19:32:47 +0530

    2. SimpleProgram

---
+++ SimpleProgram.java
@@ -0,0 +1,17 @@
+package Welcome;
+
+public class SimpleProgram 
+	{
+
+	public static void main(String[] args) 
+	{
+		int NumberOfBaskets=10, EggsPerBaskets=6, TotalEggs;
+		TotalEggs=NumberOfBaskets*EggsPerBaskets;
+		System.out.println("If you have,");
+		System.out.println(EggsPerBaskets + " Eggs per Basket");
+		System.out.println(NumberOfBaskets + " Baskets present, then");
+		System.out.println("The Total no. of eggs is " + TotalEggs+".");
+
+	}
+
+}
